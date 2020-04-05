81ccabcaf4ef02bdac8525f783ef5aea1f659051
gabioved10 <gabioved10@gmail.com>
2020-04-05 15:27:59 +0300

    N11 ver1

---
+++ src/app/user.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import * as firebase from 'firebase/app';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor() { }
+}
